2f0ba9553a3a70a4d3befa06bcae5d72c6c2fbc5
FRoX89 <56881372+FRoX89@users.noreply.github.com>
2021-04-25 17:44:28 +0200

    Add files via upload
    The Program

---
+++ main.py
@@ -0,0 +1,64 @@
+import gi
+import time
+
+
+def time_convert(sec):
+  mins = sec // 60
+  sec = sec % 60
+  hours = mins // 60
+  mins = mins % 60
+  print("Time Lapsed = {0}:{1}:{2}".format(int(hours),int(mins),sec))
+
+
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+import os
+os.system("clear")
+clicks = 0
+
+class ButtonWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="CLICK COUNTER")
+        self.set_border_width(10)
+
+        hbox = Gtk.Box(spacing=6)
+        self.add(hbox)
+
+        button = Gtk.Button.new_with_label("Click Me")
+        button.connect("clicked", self.on_click_me_clicked)
+        hbox.pack_start(button, True, True, 0)
+
+        button = Gtk.Button.new_with_mnemonic("_hi")
+        button.connect("clicked", self.on_open_clicked)
+        hbox.pack_start(button, True, True, 0)
+
+        button = Gtk.Button.new_with_mnemonic("_bye")
+        button.connect("clicked", self.on_close_clicked)
+        hbox.pack_start(button, True, True, 0)
+
+    def on_click_me_clicked(self, button):
+
+        global clicks
+        os.system("clear")
+        clicks = clicks + 1
+        print(clicks)
+
+    def on_open_clicked(self, button):
+        print("hi")
+        
+        
+    def on_close_clicked(self, button):
+        print("bye")
+        time.sleep(0.1)
+        win.destroy()
+        
+
+
+
+
+
+win = ButtonWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
