c3449b0d51353ba025bf0d346b4daceb7c529c84
FRoX89 <56881372+FRoX89@users.noreply.github.com>
2021-04-25 17:44:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,6 @@ import gi
 import time
 
 
-def time_convert(sec):
-  mins = sec // 60
-  sec = sec % 60
-  hours = mins // 60
-  mins = mins % 60
-  print("Time Lapsed = {0}:{1}:{2}".format(int(hours),int(mins),sec))
-
-
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
